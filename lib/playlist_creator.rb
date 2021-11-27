843a50146d0226553df3a3a1509d656061d7a468
James Robertson <james@jamesrobertson.me.uk>
2021-11-27 22:49:32 +0000

    1st commit

---
+++ lib/playlist_creator.rb
@@ -0,0 +1,55 @@
+#!/usr/bin/env ruby
+
+# file: playlist_creator.rb
+
+require 'dynarex'
+require 'nokogiri'
+
+
+XSLT_DX = <<XSLT
+<?xml version='1.0' encoding='UTF-8'?>
+<xsl:stylesheet xmlns:xsl='http://www.w3.org/1999/XSL/Transform' version='1.0'>
+  <xsl:output method='xml' indent='yes' omit-xml-declaration='no'  encoding='utf-8'/>
+  <xsl:template match='tracklist'>
+  <playlist version="1" xmlns="http://xspf.org/ns/0/">
+    <tracklist>
+      <xsl:apply-templates select='summary'/>
+      <xsl:apply-templates select='records/track'/>
+    </tracklist>
+  </playlist>
+  </xsl:template>
+  <xsl:template match='tracklist/summary'/>
+  <xsl:template match='records/track'>
+    <track>
+      <title>
+        <xsl:value-of select='title'/>
+      </title>
+      <location>
+        <xsl:value-of select='location'/>
+      </location>
+    </track>
+  </xsl:template>
+</xsl:stylesheet>
+XSLT
+
+
+class PlaylistCreator
+
+  def initialize(obj)
+
+    @dx = if obj.is_a? String then
+      header = "<?dynarex schema='tracklist/track(title, location)'?>\n--+\n"
+      Dynarex.new(header + obj)
+    elsif  obj.is_a? Dynarex
+      obj
+    end
+
+  end
+
+  def to_xspf()
+    doc   = Nokogiri::XML(@dx.to_xml(pretty: true))
+    xslt  = Nokogiri::XSLT(XSLT_DX)
+    xslt.transform(doc).to_xml
+  end
+
+end
---
+++ playlist_creator.gemspec
@@ -0,0 +1,14 @@
+Gem::Specification.new do |s|
+  s.name = 'playlist_creator'
+  s.version = '0.1.0'
+  s.summary = 'Creates an XML Shareable Playlist Format (XSPF) file from plain text. '
+  s.authors = ['James Robertson']
+  s.files = Dir['lib/playlist_creator.rb']
+  s.add_runtime_dependency('dynarex', '~> 1.8', '>=1.8.27')
+  s.add_runtime_dependency('nokogiri', '~> 1.12', '>=1.12.5')
+  s.signing_key = '../privatekeys/playlist_creator.pem'
+  s.cert_chain  = ['gem-public_cert.pem']
+  s.license = 'MIT'
+  s.email = 'digital.robertson@gmail.com'
+  s.homepage = 'https://github.com/jrobertson/playlist_creator'
+end
